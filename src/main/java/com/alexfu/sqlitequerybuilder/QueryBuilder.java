fc58b49ff74e82dc8447848ea7d4dff90fe20825
Steven Wu <steven.j.wu@gmail.com>
2015-03-14 12:21:46 +1030

    Removes methods for now. They will be added back in to a new helper class.

--- src/main/java/com/alexfu/sqlitequerybuilder/QueryBuilder.java
+++ src/main/java/com/alexfu/sqlitequerybuilder/QueryBuilder.java
@@ -7,22 +7,6 @@ public abstract class QueryBuilder {
     return BUILDER;
   }
 
-  public StringBuilder addComma() {
-	  return BUILDER.append(", ");
-  }
-  
-  public StringBuilder addLeftParenthesis() {
-	  return BUILDER.append("(");
-  }
-  
-  public StringBuilder addRightParenthesis() {
-	  return BUILDER.append(")");
-  }
-  
-  public StringBuilder addSpace() {
-	  return BUILDER.append(" ");
-  }
-  
   @Override
   public String toString() {
     return BUILDER.toString().trim();
