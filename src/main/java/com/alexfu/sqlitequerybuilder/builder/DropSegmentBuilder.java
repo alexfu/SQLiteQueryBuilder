23f66e2e9b2e018e6f45b426121d0a2cf7f22962
Steven Wu <steven.j.wu@gmail.com>
2015-06-30 15:00:10 +0930

    Remove DropBuilder class and move the methods to DropSegmentBuilder class.

--- src/main/java/com/alexfu/sqlitequerybuilder/builder/DropSegmentBuilder.java
+++ src/main/java/com/alexfu/sqlitequerybuilder/builder/DropSegmentBuilder.java
@@ -1,32 +1,49 @@
 package com.alexfu.sqlitequerybuilder.builder;
 
 import com.alexfu.sqlitequerybuilder.api.DropType;
+import com.alexfu.sqlitequerybuilder.utils.StringUtils;
 
 /**
  * @author Steven Wu
  */
 public class DropSegmentBuilder extends SegmentBuilder {
 
-  public DropBuilder table(String table) {
-    return new DropBuilder(DropType.TABLE, table);
+  private boolean ifExists;
+  private DropType type;
+  private String name;
+
+  private DropSegmentBuilder(DropType type, String name) {
+    this.type = type;
+    this.name = name;
   }
 
-  public DropBuilder view(String view) {
-    return new DropBuilder(DropType.VIEW, view);
+  public DropSegmentBuilder() {
   }
 
-  public DropBuilder index(String index) {
-    return new DropBuilder(DropType.INDEX, index);
+  public DropSegmentBuilder table(String table) {
+    return new DropSegmentBuilder(DropType.TABLE, table);
   }
 
-  public DropBuilder trigger(String trigger) {
-    return new DropBuilder(DropType.TRIGGER, trigger);
+  public DropSegmentBuilder view(String view) {
+    return new DropSegmentBuilder(DropType.VIEW, view);
+  }
+
+  public DropSegmentBuilder index(String index) {
+    return new DropSegmentBuilder(DropType.INDEX, index);
+  }
+
+  public DropSegmentBuilder trigger(String trigger) {
+    return new DropSegmentBuilder(DropType.TRIGGER, trigger);
+  }
+
+  public DropSegmentBuilder ifExists() {
+    ifExists = true;
+    return this;
   }
 
   @Override
   public String build() {
-    // TODO Auto-generated method stub
-    return null;
+    return StringUtils.join(" ", "DROP", type, (ifExists ? "IF EXISTS" : ""), name);
   }
-
+  
 }
