694962a7d5a09a8e9e9ebc00be4115a6f6266b24
Alex Fu <alex.fu914@gmail.com>
2015-07-28 07:54:47 -0400

    Fix indentations

--- src/test/java/com/alexfu/sqlitequerybuilder/DeleteTest.java
+++ src/test/java/com/alexfu/sqlitequerybuilder/DeleteTest.java
@@ -12,9 +12,9 @@ public class DeleteTest {
   @Test
   public void testDeleteAll() {
     String sql = SQLiteQueryBuilder
-        .delete()
-        .from("my_table")
-        .build();
+      .delete()
+      .from("my_table")
+      .build();
 
     assertThat(sql).isEqualTo("DELETE FROM my_table");
   }
@@ -22,10 +22,10 @@ public class DeleteTest {
   @Test
   public void testDeleteWhere() {
     String sql = SQLiteQueryBuilder
-        .delete()
-        .from("my_table")
-        .where("id = 1")
-        .build();
+      .delete()
+      .from("my_table")
+      .where("id = 1")
+      .build();
 
     assertThat(sql).isEqualTo("DELETE FROM my_table WHERE id = 1");
   }
