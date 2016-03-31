63392724af2d30bdded4ab6572b17bc8483b3411
Alex Fu <alex.fu914@gmail.com>
2016-03-30 22:20:15 -0400

    Clean up imports

--- src/test/java/com/alexfu/sqlitequerybuilder/CreateTableTest.java
+++ src/test/java/com/alexfu/sqlitequerybuilder/CreateTableTest.java
@@ -1,20 +1,16 @@
 package com.alexfu.sqlitequerybuilder;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import com.alexfu.sqlitequerybuilder.utils.ConnectionUtils;
-import org.junit.Test;
-
 import com.alexfu.sqlitequerybuilder.api.Column;
 import com.alexfu.sqlitequerybuilder.api.ColumnConstraint;
 import com.alexfu.sqlitequerybuilder.api.ColumnType;
 import com.alexfu.sqlitequerybuilder.api.SQLiteQueryBuilder;
+import com.alexfu.sqlitequerybuilder.utils.ConnectionUtils;
+import org.junit.Test;
 
-import java.sql.DatabaseMetaData;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public final class CreateTableTest extends SQLiteTest {
 
