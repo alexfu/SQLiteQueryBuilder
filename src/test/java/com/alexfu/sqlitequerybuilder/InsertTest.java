aeb347a402878f96de72683d70d769fe97f93dcc
Alex Fu <alex.fu914@gmail.com>
2016-04-07 08:14:21 -0400

    Refactor InsertTest

--- src/test/java/com/alexfu/sqlitequerybuilder/InsertTest.java
+++ src/test/java/com/alexfu/sqlitequerybuilder/InsertTest.java
@@ -4,23 +4,44 @@ import com.alexfu.sqlitequerybuilder.api.SQLiteQueryBuilder;
 import com.alexfu.sqlitequerybuilder.utils.DateUtils;
 import org.junit.Test;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Calendar;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class InsertTest {
+public final class InsertTest extends SQLiteTest {
+
+  @Override
+  public void before() throws SQLException, ClassNotFoundException {
+    super.before();
+
+    // Seed database
+    String sql = "CREATE TABLE people (id INTEGER, name TEXT, timestamp TEXT)";
+    statement.execute(sql);
+  }
+
   @Test
-  public void insertWithGoodValues() {
+  public void insertWithGoodValues() throws SQLException {
+    // Arrange
     Calendar timestamp = Calendar.getInstance();
     String sql = SQLiteQueryBuilder.insert()
       .into("people")
       .columns("id", "name", "timestamp")
-      .values(1, "John", Calendar.getInstance())
+      .values(1, "John", timestamp)
       .build();
 
-    String iso8601 = DateUtils.iso8601().format(timestamp.getTime());
-    String result = "INSERT INTO people (id,name,timestamp) values (1,'John','" + iso8601 + "')";
-    assertThat(sql).isEqualTo(result);
+    // Act
+    statement.execute(sql);
+
+    // Assert
+    ResultSet set = statement.executeQuery("SELECT * FROM people");
+    set.next();
+    assertThat(set.getInt("id")).isEqualTo(1);
+    assertThat(set.getString("name")).isEqualTo("John");
+
+    String date = DateUtils.iso8601().format(timestamp.getTime());
+    assertThat(set.getString("timestamp")).isEqualTo(date);
   }
 
   @Test(expected = IllegalArgumentException.class)
